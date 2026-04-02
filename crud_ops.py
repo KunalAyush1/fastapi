f35b497d77ba319cb27a57038a01e1b32ae2a049
Kunal <singhkunalakhilesh2@gmail.com>
2026-04-03 03:28:41 +0530

    Delete opn

--- crud_ops.py
+++ crud_ops.py
@@ -22,6 +22,12 @@ def find_post(id):
         if p["id"] == id:
             return p
 
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i #returns index of that specific id
+            
+
 
 @app.get("/posts")
 def get_posts():
@@ -29,8 +35,8 @@ def get_posts():
 
 
 
-@app.post("/posts")
-def create_post(post: Post, status_code = status.HTTP_201_CREATED): #if we want to throw a specific HTTP status code , pass it as parameter in the function
+@app.post("/posts", status_code = status.HTTP_201_CREATED)#if we want to throw a specific HTTP status code , pass it as parameter in the decorator
+def create_post(post: Post): 
     post_dict = post.dict()
     post_dict['id'] = randrange(0,1000000)
     my_posts.append(post_dict)
@@ -52,3 +58,10 @@ def get_post(id: int, response: Response):  #validation -> id is getting convert
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {id} was not found")
     return {"post_detail": f"Here is post {id}"}
 
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
+    my_posts.pop(index)
+    return Response(status_code = status.HTTP_204_NO_CONTENT)
